3db4b5ee92fdd06faa6a70ea735d9ea3abb0d74e
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-07-02 20:21:40 -0500

    log in using API complete

--- src/Context/Auth/index.jsx
+++ src/Context/Auth/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import testUsers from './lib/user'
 import jwt_decode from 'jwt-decode'
 import cookie from 'react-cookies';
+import axios from 'axios';
 
 export const AuthContext = React.createContext()
 
@@ -31,16 +32,39 @@ export default function AuthProvider({ children }) {
     }
   }
 
-  const login = (username, password) => {
-    let user = testUsers[username]
-    if (user && password === user.password) {
+  // const login = (username, password) => {
+  //   let user = testUsers[username]
+  //   if (user && password === user.password) {
+  //     try {
+  //       validateToken(user.token)
+  //     } catch (err) {
+  //       console.error('ERROR VALIDATING', err)
+  //     }
+  //   }
+  // }
+
+  // USING API to sign in now instead of the users.js
+  const login = async (username, password) => {
+    let config = {
+      baseURL: 'https://api-js401.herokuapp.com',
+      url: '/signin',
+      method: 'post',
+      auth: { username, password }
+    }
+
+    let response = await axios(config);
+    console.log('RESPONSE FROM AXIOS--------------', response.data);
+    let token = response.data.token;
+
+    if (token) {
       try {
-        validateToken(user.token)
-      } catch (err) {
-        console.error('ERROR VALIDATING', err)
+        validateToken(token);
+      } catch (error) {
+        setError(error);
+        console.log('Login Error', error);
       }
     }
-  }
+  };
 
   const logout = () => {
     setUser({})
